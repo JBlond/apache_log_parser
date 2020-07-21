9af5434142c0d64edf6abb6428f96f795d137c7e
Alex McKee <alex.mckee@mcsuk.org>
2020-07-21 13:48:12 +0100

    Add autoloading via composer.

--- error_log.php
+++ error_log.php
@@ -2,8 +2,7 @@
 
 use jblond\Apache_Log_Parser\ApacheErrorLogParser;
 
-require 'ApacheLogParser.php';
-require 'ApacheErrorLogParser.php';
+require 'vendor/autoload.php';
 
 $log_parser = new ApacheErrorLogParser("C:\\Users\\jblond\\Apache24\\logs\\error.log");
 echo $log_parser->output();
